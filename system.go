8e6a7e17eed10780e5c61ab2997f841dcf0cff89
Crumbed <kaiharrelson5466@gmail.com>
2025-03-28 13:00:42 -0400

    Updated systems

--- system.go
+++ system.go
@@ -3,25 +3,27 @@ package ecs
 
 
 
-
 type System struct {
+    // Tick interval between executions
     rate    uint
+    // Number of ticks elapsed since last execution
     passed  uint
-    query   ComponentHash
-    Fn      func(*ECS)
+    Query   Query
+    // Function to execute
+    Fn      func(*ECS, []EntityHandle)
 }
 
 func (s *System) Check(ecs *ECS) {
     s.passed += 1
     if s.passed != s.rate { return }
     s.passed = 0
-    s.Fn(ecs)
+    s.Fn(ecs, s.Query.Apply(ecs))
 }
 
-func NewSystem(rate uint, query ComponentHash, fn func(*ECS)) System {
+func NewSystem(rate uint, query Query, fn func(*ECS, []EntityHandle)) System {
     return System {
         rate: rate,
-        query: query,
+        Query: query,
         Fn: fn,
     }
 }
